9d1e67316056589ad11c06bd72a0bdc4c9f071c5
Okan <okann.alkann@gmail.com>
2021-10-18 21:19:49 +0300

    heroku deployment

--- helper/db.js
+++ helper/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://alkano:alkano@movie-api-2021.mcg1i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useMongoClient : true});
+    mongoose.connect('mongodb+srv://alkano:alkano@movie-api-2021.mcg1i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' /*, {useMongoClient : true}*/);
     mongoose.connection.on('open', () => {
-        console.log('Baglandi');
+        //console.log('Baglandi');
     })
 
     mongoose.connection.on('error', (err) => {
